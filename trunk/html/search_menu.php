f495143fe7b0d50f708380d3c4d175c056602338
forstall <forstall@755e928b-599d-464e-84aa-334d8f2b6545>
2012-01-07 05:46:29 +0000

    git-svn-id: svn://tesserae.caset.buffalo.edu/tesserae@58 755e928b-599d-464e-84aa-334d8f2b6545

--- trunk/html/search_menu.php
+++ trunk/html/search_menu.php
@@ -2,7 +2,7 @@
 	<ul>
 		<li><a href="<?php echo $url_html.'/index.php' ?>">Basic Search</a></li>
 		<li><a href="<?php echo $url_html.'/la_table.php' ?>">Advanced Features</a></li>
-		<li><a href="<?php echo $url_html.'/grc_table.php' ?>">Greek</a></li>
+		<li><a href="<?php echo $url_html.'/grc_table.php' ?>">Greek Search</a></li>
 		<li><a href="<?php echo $url_html.'/full-text.php' ?>">Full-text display</a></li>
 	</ul>
 </div>
