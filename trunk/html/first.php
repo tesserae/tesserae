732544bbaecd3bb15fa3e2a79bbd9338fa15ec0b
forstall <forstall@755e928b-599d-464e-84aa-334d8f2b6545>
2012-03-05 22:04:29 +0000

    git-svn-id: svn://tesserae.caset.buffalo.edu/tesserae@82 755e928b-599d-464e-84aa-334d8f2b6545

--- trunk/html/first.php
+++ trunk/html/first.php
@@ -5,7 +5,7 @@
 <?php $url_cgi="http://localhost/~chris/tesserae/cgi-bin" ?><!-- URL_CGI -->
 <?php $url_image="http://localhost/~chris/tesserae/images" ?><!-- URL_IMAGE -->
 <?php $url_text="http://localhost/~chris/tesserae/texts" ?><!-- URL_TEXT -->
-<?php $fs_html="/Users/chris/Sites/tesserae/html" ?><!-- FS_HTML -->
+<?php $fs_html="/Users/chris/tesserae/html" ?><!-- FS_HTML -->
 
 
 <html lang="en">
