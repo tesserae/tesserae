7205e7a741d443cd7d69be821d7781ef4b0c0463
Chris Forstall <forstall@buffalo.edu>
2013-09-05 15:00:04 -0400

    add cwf trans feature only sort of works

--- scripts/synonymy/build-trans-cache.pl
+++ scripts/synonymy/build-trans-cache.pl
@@ -1,27 +1,28 @@
 #!/usr/bin/env perl
 
-#
-# corpus-stats.pl
-#
-# create lists of most frequent tokens by rank order
-# in order to calculate stop words
-# and frequency-based scores
-
 =head1 NAME
 
-corpus-stats.pl	- calculate corpus-wide statistics
+build-trans-cache.pl - install translation dictionary
 
 =head1 SYNOPSIS
 
-perl corpus-stats.pl [options] LANG [LANG2 [...]]
+build-trans-cache.pl [--feature NAME] DICT
 
 =head1 DESCRIPTION
 
-Calculates corpus-wide frequencies for all features.
+Reads a translation dictionary in CSV format; creates and installs Tesserae dictionary in Storable binary format. Dictionaries should be utf-8 encoded text, with one line per headword. Each line begins with the Greek headword to be translated, followed by one or more Latin headwords to be considered "translations" of the Greek. Fields must be separated by commas.
 
 =head1 OPTIONS AND ARGUMENTS
 
 =over
+
+=item I<DICT>
+
+Dictionary file to read.
+
+=item B<--feature> NAME
+
+Optional name for feature set. Default is "trans".
 
 =item B<--help>
 
@@ -33,8 +34,6 @@ Print usage and exit.
 
 =head1 SEE ALSO
 
-I<add_column.pl> - add texts to the database
-
 =head1 COPYRIGHT
 
 University at Buffalo Public License Version 1.0.
@@ -42,13 +41,13 @@ The contents of this file are subject to the University at Buffalo Public Licens
 
 Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the specific language governing rights and limitations under the License.
 
-The Original Code is corpus-stats.pl.
+The Original Code is build-trans-cache.pl.
 
 The Initial Developer of the Original Code is Research Foundation of State University of New York, on behalf of University at Buffalo.
 
 Portions created by the Initial Developer are Copyright (C) 2007 Research Foundation of State University of New York, on behalf of University at Buffalo. All Rights Reserved.
 
-Contributor(s): Chris Forstall
+Contributor(s): Chris Forstall, James Gawley
 
 Alternatively, the contents of this file may be used under the terms of either the GNU General Public License Version 2 (the "GPL"), or the GNU Lesser General Public License Version 2.1 (the "LGPL"), in which case the provisions of the GPL or the LGPL are applicable instead of those above. If you wish to allow use of your version of this file only under the terms of either the GPL or the LGPL, and not to allow others to use your version of this file under the terms of the UBPL, indicate your decision by deleting the provisions above and replace them with the notice and other provisions required by the GPL or the LGPL. If you do not delete the provisions above, a recipient may use your version of this file under the terms of any one of the UBPL, the GPL or the LGPL.
 
@@ -122,21 +121,27 @@ use Pod::Usage;
 
 # load additional modules necessary for this script
 
-use Storable qw(nstore retrieve);
+use Storable qw/nstore/;
+use Encode;
+use File::Copy;
+use Unicode::Normalize;
+use utf8;
 
 # initialize some variables
 
-my @feature = qw/word/;
-my @lang;
-my $help = 0;
+my $feat  = 'trans';
+my $help  = 0;
 my $quiet = 0;
+
+my $re_dia   = qr/[\x{0313}\x{0314}\x{0301}\x{0342}\x{0300}\x{0308}\x{0345}]/;
+my $re_vowel = qr/[αειηουωΑΕΙΗΟΥΩ]/;
 
 # get user options
 
 GetOptions(
+	'feature=s' => \$feat,
 	'help'      => \$help,
-	'quiet'     => \$quiet,
-	'feature=s' => \@feature
+	'quiet'     => \$quiet
 );
 
 # print usage if the user needs help
@@ -146,96 +151,88 @@ if ($help) {
 	pod2usage(1);
 }
 
-#
-# allow utfu output to STDERR
-#
+# Get filename from user arg
 
-binmode STDERR, ':utf8';
+my $file = shift @ARGV;
 
-#
-# specify language to parse at cmd line
-#
-
-for (@ARGV) {
-
-	$_ = lc($_);
-
-	if (/^[a-z]{1,4}$/)	{ 
+unless ($file) {
 	
-		next unless -d catdir($fs{data}, 'v3', $_);
-		push @lang, $_;
-	}
+	warn "Please specify CSV dictionary to read.";
+	pod2usage(1);
 }
 
-print STDERR "lang: " . join(" ", @lang) . "\n" unless $quiet;
-print STDERR "feature: " . join(" ", @feature) . "\n" unless $quiet;
+binmode STDOUT, ':utf8';
 
 #
-# main loop
+
+
+# parse the csv dictionary
 #
 
-# word counts come from documents already parsed.
-# stem counts are based on word counts, but also 
-# use the cached stem dictionary
-#
+my %trans;
 
-for my $lang (@lang) {
+open (my $fh, '<:utf8', $file) or die "Can't read dictionary $file: $!";
+
+print STDERR "Parsing $file\n" unless $quiet;
+
+my $pr = ProgressBar->new(-s $file, $quiet);
+
+while (my $line = <$fh>) {
+
+	$pr->advance(length(Encode::encode('utf8', $line)));
+
+	chomp $line;
 	
-	# get a list of all the word counts
-
-	my @texts = @{Tesserae::get_textlist($lang, -no_part => 1)};
+	my ($head, @trans) = split(/\s*,\s*/, $line);
 	
-	#
-	# combine the counts for each file to get a corpus count
-	#
-
-	my %total;
-	my %count;
-
-	for my $text (@texts) {
+	$head = NFKD($head);
+	$head =~ s/\d//g;
+	$head =~ s/^(${re_dia}+)(${re_vowel}{2,})/$2/;
+	$head =~ s/^(${re_dia}+)(${re_vowel}{1})/$2$1/;
+	$head =~ s/σ\b/ς/;
 	
-		print STDERR "checking $text:";
-		
-		for my $feature (@feature) {
-		
-			my $file_index = catfile($fs{data}, 'v3', $lang, $text, "$text.index_$feature");
-
-			next unless -s $file_index;
-
-			my %index = %{retrieve($file_index)};
-
-			print STDERR " $feature";
-
-			for (keys %index) { 
-			
-				$count{$feature}{$_} += scalar(@{$index{$_}});
-				$total{$feature}     += scalar(@{$index{$_}});
-			}
-		}
-		
-		print STDERR "\n";
-	}
-
-	# after the whole corpus is tallied,	
-	# convert counts to frequencies and save
+	@trans = grep { /\S/ } @trans;
 	
-	for my $feature (@feature) {
+	next unless @trans;
 	
-		next unless defined $count{$feature};
-
-		my $file_freq = catfile($fs{data}, 'common', $lang . '.' . $feature . '.freq');
-
-		print STDERR "writing $file_freq\n";
-
-		open (FREQ, ">:utf8", $file_freq) or die "can't write $file_freq: $!";
-
-		print FREQ "# count: $total{$feature}\n";
-		
-		for (sort {$count{$feature}{$b} <=> $count{$feature}{$a}} keys %{$count{$feature}}) {
-		
-			print FREQ "$_\t$count{$feature}{$_}\n";
-		}
-
-		close FREQ;
-	}
+	push @{$trans{$head}}, @trans;
+	
+	print "$head: " . join(" ", @trans) . "\n";
 }
+
+#
+# save as Storable binary
+#
+
+my $file_cache = catfile($fs{data}, 'common', "grc.$feat.cache");
+
+print STDERR "Writing $file_cache\n" unless $quiet;
+
+nstore(\%trans, $file_cache);
+
+#
+# copy the Latin stem dictionary
+#
+
+my $from = catfile($fs{data}, 'common', 'la.stem.cache');
+my $to   = catfile($fs{data}, 'common', "la.$feat.cache");
+
+unless (-s $from) {
+
+	my $warning = <<END;
+Can't find Latin stem cache $from!
+Without Latin stems the translation feature will not work!
+Please run build-stem-cache.pl, then either re-run this script
+or copy $from to $to yourself.
+END
+
+	warn $warning;
+	die "No stem dictionary, can't continue.";
+}
+
+print STDERR "Writing $to\n" unless $quiet;
+
+copy($from, $to);
+
+
+
