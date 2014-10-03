010ad3dc5e25149bc8bccd3a9098ee25662ce706
Gawley <jamesgaw@cast-clgenusem.caset.buffalo.edu>
2014-10-03 15:54:15 -0400

    Added proper names to the diagnostic options.

--- cgi-bin/syn-diagnostic-lookup.pl
+++ cgi-bin/syn-diagnostic-lookup.pl
@@ -472,7 +472,7 @@ sub format_query {
 					<option value="adjt">adjective</option>					
 					<option value="pron">pronoun</option>
 					<option value="advb">adverb</option>
-					<option value="advb">name</option>
+					<option value="name">name</option>
 					<option value="unkn">ambiguous</option>
 				</select>
 			</div>
