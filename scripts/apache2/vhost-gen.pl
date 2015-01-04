4a7cdd18c02e873b08cbfe11b2b18fa4776b4831
Chris Forstall <cforstall@gmail.com>
2015-01-05 00:10:32 +0100

    tweak doc-gen

--- scripts/apache2/vhost-gen.pl
+++ scripts/apache2/vhost-gen.pl
@@ -158,6 +158,13 @@ print <<END;
 		Order allow,deny
 		Allow from All		
 	</Directory>
+   
+   Alias /doc/ $fs{doc}/
+   <Directory "$fs{doc}">
+      AllowOverride None
+      Order allow,deny
+      Allow from All
+   </Directory>
 		
 	Alias /css/ $fs{css}/
 	<Directory "$fs{css}">
@@ -181,13 +188,13 @@ print <<END;
 		Allow from all
 	</Directory>
 
-	ErrorLog \${APACHE_LOG_DIR}/error.log
+	ErrorLog \${APACHE_LOG_DIR}/tesserae.error.log
 
 	# Possible values include: debug, info, notice, warn, error, crit,
 	# alert, emerg.
 	LogLevel warn
 
-	CustomLog \${APACHE_LOG_DIR}/access.log combined
+	CustomLog \${APACHE_LOG_DIR}/tesserae.access.log combined
 
 </VirtualHost>
 
