25c9545cf3a12c78ae5178b2cd5c38a2555719a0
Chris Forstall <cforstall@gmail.com>
2015-01-08 00:23:31 +0100

    update documentation

--- cgi-bin/read_table.pl
+++ cgi-bin/read_table.pl
@@ -6,97 +6,164 @@ read_table.pl - Perform a Tesserae search.
 
 =head1 SYNOPSIS
 
-B<read_table.pl> B<--target> I<target_text> B<--source> I<source_text> [OPTIONS]
+read_table.pl --target I<target_text> --source I<source_text>
+[OPTIONS]
 
 =head1 DESCRIPTION
 
-This script compares two texts in the Tesserae corpus and returns a list of "parallels", pairs of textual units which share common features.  These parallels are organized in a set of hashes which are saved as a binaries using Storable.  These files, kept together in a directory named for the session, can be read and formatted in a user-friendly way with the companion script I<read_bin.pl>.
+This script compares two texts in the Tesserae corpus and returns a list of
+"parallels", pairs of textual units which share common features. These
+parallels are organized in a set of hashes which are saved as a binaries
+using Storable. These files, kept together in a directory named for the
+session, can be read and formatted in a user-friendly way with the companion
+script B<read_bin.pl>.
 
-This script is primarily called as a cgi executable from the web interface.  Called as a cgi, it creates a new session id for the results and saves them to the Tesserae I<tmp/> directory.  It then redirects the browser to I<read_bin.pl> which mediates viewing the results.
+This script is primarily called as a cgi executable from the web interface.
+Called as a cgi, it creates a new session id for the results and saves them
+to the Tesserae F<tmp/> directory. It then redirects the browser to
+B<read_bin.pl> which mediates viewing the results.
 
-It can also be run from the command line.  In this case, the results are written to a new directory given a user-specified session name (or "tesresults" by default).
+It can also be run from the command line. In this case, the results are
+written to a new directory given a user-specified session name (or
+"tesresults" by default).
 
-The names of the source and target texts to be searched must be specified.  B<Target> means the alluding (more recent) text.  B<Source> is the alluded-to (earlier) text.
+The names of the source and target texts to be searched must be specified.
+B<Target> means the alluding (more recent) text. B<Source> is the alluded-to
+(earlier) text.
 
-The name of a text is identical to its filename without the C<.tess> extension.  For example, our benchmark test is to search for allusions to Vergil's Aeneid in Book 1 of Lucan's Bellum Civile.  The file containing the Aeneid is I<texts/la/vergil.aeneid.tess> and that containing just the first book of Pharsalia is I<texts/la/lucan.bellum_civile/lucan.bellum_civile.part.1.tess>.  Thus, a default search, taking Lucan as the alluder and Vergil as the alluded-to, is run like this:
+The name of a text is identical to its filename without the F<.tess>
+extension. For example, our benchmark test is to search for allusions to
+Vergil's Aeneid in Book 1 of Lucan's Bellum Civile. The file containing the
+Aeneid is F<texts/la/vergil.aeneid.tess> and that containing just the first
+book of Pharsalia is
+F<texts/la/lucan.bellum_civile/lucan.bellum_civile.part.1.tess>. Thus, a
+default search, taking Lucan as the alluder and Vergil as the alluded-to, is
+run like this:
 
-% cgi-bin/read_table.pl --source vergil.aeneid \	
-                        --target lucan.bellum_civile.part.1
+ % cgi-bin/read_table.pl --source vergil.aeneid \
+                         --target lucan.bellum_civile.part.1
 
-=head1 OPTIONS 
+=head1 OPTIONS
 
 =over
 
-=item B<--unit> line|phrase
+=item --unit <line|phrase>
 
-I<unit> specifies the textual units to be compared.  Choices currently are B<line> (the default) which compares verse lines or B<phrase>, which compares grammatical phrases.  For now we assume that the punctuation marks [.;:?] delimit phrases.
+Specifies the textual units to be compared. Choices currently are
+B<line> (the default) which compares verse lines or B<phrase>, which
+compares grammatical phrases. For now we assume that the punctuation marks
+[.;:?] delimit phrases.
 
-=item B<--feature> word|stem|syn 
+=item --feature <word|stem|syn>
 
-This specifies the features set to match against.  B<word> only allows matches on forms that are identical. B<stem> (the default), allows matches on any inflected form of the same stem. B<syn> matches not only forms of the same headword but also other headwords taken to be related in meaning.  B<stem> and B<syn> only work if the appropriate dictionaries are installed; B<syn> won't work on Greek or English.
+This specifies the features set to match against. B<word> only allows
+matches on forms that are identical. B<stem> (the default), allows matches
+on any inflected form of the same stem. B<syn> matches not only forms of the
+same headword but also other headwords taken to be related in meaning.
+B<stem> and B<syn> only work if the appropriate dictionaries are installed.
 
-=item B<--stop> I<stoplist_size>
+=item --stop I<N>
 
-I<stoplist_size> is the number of stop words (stems, etc.) to use.  Matches on any of these are excluded from results.  The stop list is calculated by ordering all the features (see above) in the stoplist basis (see below) by frequency and taking the top I<N>, where I<N>=I<stoplist_size>.  The default is 10.
+The number of stop words (stems, etc.) to use. Matches on any of these are
+excluded from results. The stop list is calculated by ordering all the 
+features (see above) in the stoplist basis (see below) by frequency and taking
+the top I<N>. The default is 10.
 
-=item B<--stbasis> corpus|target|source|both
+=item --stbasis <corpus|target|source|both>
 
-Stoplist basis is a string indicating the source for the ranked list of features from which the stoplist is taken.  B<corpus> (the default) derives the stoplist from the entire corpus; B<source>, uses only the source; B<target>, only the target; and B<both> uses the source and target but nothing else.
+A string indicating the source for the ranked list of features from which the
+stoplist is taken. B<corpus> derives the stoplist from the entire corpus; 
+B<source>, uses only the source; B<target>, only the target; and B<both> (the
+default) uses the source and target but nothing else.
 
-=item B<--dist> I<max_dist>
+=item --dist I<N>
 
-This sets the maximum distance between matching words.  For two units (one in the source and one in the target) to be considered a match, each must have at least two words common to the other (regardless of the feature on which they matched).  It's generally true that in good allusions these words are close together in both units.  Setting the maximum distance to I<N> means that matches where either unit's matching words are more than I<N> words apart will be excluded. The default distance is 999, which is presumably equivalent to setting no limit. Note that adjacent words are considered to have a distance of 1, words separated by an intervening word have a distance of 2, and so on.
+This sets the maximum distance between matching words. For two units (one in
+the source and one in the target) to be considered a match, each must have
+at least two words common to the other (regardless of the feature on which
+they matched). It's generally true that in good allusions these words are
+close together in both units. Setting the maximum distance to I<N> means
+that matches where the sum of the distances in the two matched phrases
+exceeds I<N> words will be excluded. The default distance is 10. If you don't
+want to limit distance, set I<N> to something bigger than the number of
+tokens in a unit, e.g., 999. Note that adjacent words are considered to
+have a distance of 1, words separated by a single intervening word have a 
+distance of 2, and so on.
 
-=item B<--dibasis> span|span-target|span-source|freq|freq-target|freq-source
+=item --dibasis <span|freq>
 
-Distance basis is a string indicating the way to calculate the distance between matching words in a parallel (matching pair of units).  B<span> adds together the distance in words between the two farthest-apart words in each phrase.  Related to this are B<span-target> which uses the distance between the two farthest-apart words in the target unit only, and B<span-source> which uses the two farthest-apart words in the source unit.  A (probably) better basis is B<freq>, which uses the distance between the two words with the lowest frequencies (in their own text only), adding the frequency-based distances of the target and source units together.  As for B<span>, you can select the frequency-based distance in only one text with B<freq-target> or B<freq-source>.  The default is B<freq>.
+Distance basis is a string indicating the way to calculate the distance
+between matching words in a parallel (matching pair of units). B<span> adds
+together the distance in words between the two farthest-apart words in each
+phrase. The default option is B<freq>, which uses the distance between the
+two words with the lowest frequencies (in their own text only), adding the
+frequency-based distances of the target and source units together.
 
-=item B<--cutoff> I<score_cutoff>
+=item --cutoff I<N>
 
-Each match found by Tesserae is given a score.  Setting a cutoff will cause any match with a score less than this to be dropped from the results.  Default is 0 (presumably equivalent to no cutoff).
+Each match found by Tesserae is given a score. Setting a cutoff will cause
+any match with a score less than this to be dropped from the results.
+Default is 0 (presumably equivalent to no cutoff).
 
-=item B<--binary> I<name>
+=item --binary I<name>
 
-This is the name to be given to the session. Tesserae will create a new directory with this name and save there the Storable binaries containing your results.  The default is I<tesresults>.
+This is the name to be given to the session. Tesserae will create a new
+directory with this name and save there the Storable binaries containing
+your results. The default is B<tesresults>.
 
-=item B<--quiet>
+=item --quiet
 
 Don't write progress info to STDERR.
 
-=item B<--help>
+=item --help
 
 Print this message and exit.
 
 =back
 
-The values of all these options should be printed to STDERR when you run the script from the command-line, and should also be saved with the results.
+The values of all these options should be printed to STDERR when you run the
+script from the command-line, and should also be saved with the results.
 
 =head1 KNOWN BUGS
 
-Right now the script also prints benchmark information to STDOUT.  This consists of a couple of messages about how many seconds different parts of the script take.  You can't turn it off, but you could always redirect it to /dev/null.
-
-Also, I'm not sure what will happen if you select a feature that hasn't been installed.
-
 =head1 SEE ALSO
 
-I<cgi-bin/read_table.pl>
+B<cgi-bin/read_bin.pl>
 
 =head1 COPYRIGHT
 
-University at Buffalo Public License Version 1.0.
-The contents of this file are subject to the University at Buffalo Public License Version 1.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://tesserae.caset.buffalo.edu/license.txt.
+University at Buffalo Public License Version 1.0. The contents of this file
+are subject to the University at Buffalo Public License Version 1.0 (the
+"License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://tesserae.caset.buffalo.edu/license.txt.
 
-Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the specific language governing rights and limitations under the License.
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+the specific language governing rights and limitations under the License.
 
 The Original Code is read_table.pl.
 
-The Initial Developer of the Original Code is Research Foundation of State University of New York, on behalf of University at Buffalo.
+The Initial Developer of the Original Code is Research Foundation of State
+University of New York, on behalf of University at Buffalo.
 
-Portions created by the Initial Developer are Copyright (C) 2007 Research Foundation of State University of New York, on behalf of University at Buffalo. All Rights Reserved.
+Portions created by the Initial Developer are Copyright (C) 2007 Research
+Foundation of State University of New York, on behalf of University at
+Buffalo. All Rights Reserved.
 
-Contributor(s): Neil Coffee, Chris Forstall, James Gawley.
+Contributor(s): Chris Forstall <cforstall@gmail.com>, James Gawley.
 
-Alternatively, the contents of this file may be used under the terms of either the GNU General Public License Version 2 (the "GPL"), or the GNU Lesser General Public License Version 2.1 (the "LGPL"), in which case the provisions of the GPL or the LGPL are applicable instead of those above. If you wish to allow use of your version of this file only under the terms of either the GPL or the LGPL, and not to allow others to use your version of this file under the terms of the UBPL, indicate your decision by deleting the provisions above and replace them with the notice and other provisions required by the GPL or the LGPL. If you do not delete the provisions above, a recipient may use your version of this file under the terms of any one of the UBPL, the GPL or the LGPL.
+Alternatively, the contents of this file may be used under the terms of
+either the GNU General Public License Version 2 (the "GPL"), or the GNU
+Lesser General Public License Version 2.1 (the "LGPL"), in which case the
+provisions of the GPL or the LGPL are applicable instead of those above. If
+you wish to allow use of your version of this file only under the terms of
+either the GPL or the LGPL, and not to allow others to use your version of
+this file under the terms of the UBPL, indicate your decision by deleting
+the provisions above and replace them with the notice and other provisions
+required by the GPL or the LGPL. If you do not delete the provisions above,
+a recipient may use your version of this file under the terms of any one of
+the UBPL, the GPL or the LGPL.
 
 =cut
 
--- html/first.php
+++ html/first.php
@@ -30,8 +30,7 @@
 			</div>		
 			<div id="nav_main">
 				<ul>
-					<li><a href="<?php echo $url_html; ?>">Search</a></li>
-					<li><a href="<?php echo $url_html . "/help.php"; ?>">Help</a></li>
+					<li><a href="<?php echo $url_doc . "/"; ?>">Help</a></li>
 					<li><a href="http://tesserae.caset.buffalo.edu/blog">Blog</a></li>
 
 				</ul>
