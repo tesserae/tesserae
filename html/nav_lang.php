159981f1bc986e7dad9a69478fdb0d48f9f2aa67
jamesgawley <james.gawley@gmail.com>
2015-01-14 10:55:35 -0500

    Trying a new tack with sub-menus.

--- html/nav_lang.php
+++ html/nav_lang.php
@@ -1,4 +1,13 @@
 			<div id="nav_sub2">
+				<div id ="nav_sub">
+				<ul>
+					<li><a href="<?php echo $url_html.'/index.php' ?>">Search</a></li>
+					<li><a href="<?php echo $url_html.'/experimental.php' ?>">Other Tools</a></li>
+					<li><a href="<?php echo $url_html.'/sources.php'; ?>">Sources</a></li>
+					<li><a href="<?php echo $url_html.'/help.php'; ?>">Help</a></li>					
+					<li><a href="http://tesserae.caset.buffalo.edu/blog" target="_blank">Blog</a></li>					
+				</ul>
+				</div>
 				<ul>
 					<li><a href="<?php echo $url_html.'/index.php ?>">Latin</a></li>
 					<li><a href="<?php echo $url_html.'/grc_table.php' ?>">Greek</a></li>
