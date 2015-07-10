4f2f1f9c7205a8e71f814cd7cb3d4983f99c74fa
Chris Forstall <cforstall@gmail.com>
2015-07-10 09:40:08 +0200

    couple of bugfixes

--- scripts/install.pl
+++ scripts/install.pl
@@ -10,13 +10,11 @@ install.pl [options]
 
 =head1 DESCRIPTION
 
-This script is to be run after configure.pl. Program components are
-moved from default locations to those specified in tesserae.conf.
-Pointer files back to tesserae.conf are placed in the newly-installed
-directories. 
+This script is to be run after configure.pl. Program components are moved from
+default locations to those specified in tesserae.conf. Pointer files back to
+tesserae.conf are placed in the newly-installed directories.
 
-This script also generates defs.php, which is necessary for the web
-interface.
+This script also generates defs.php, which is necessary for the web interface.
 
 =head1 OPTIONS AND ARGUMENTS
 
@@ -30,27 +28,45 @@ Print usage and exit.
 
 =head1 KNOWN BUGS
 
-Doesn't actually move any files. If you want them installed other than
-in the default locations, you have to move them yourself.
+Doesn't actually move any files. If you want them installed other than in the
+default locations, you have to move them yourself.
 
 =head1 SEE ALSO
 
 =head1 COPYRIGHT
 
-University at Buffalo Public License Version 1.0.
-The contents of this file are subject to the University at Buffalo Public License Version 1.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://tesserae.caset.buffalo.edu/license.txt.
+University at Buffalo Public License Version 1.0. The contents of this file are
+subject to the University at Buffalo Public License Version 1.0 (the
+"License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://tesserae.caset.buffalo.edu/license.txt.
 
-Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the specific language governing rights and limitations under the License.
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+the specific language governing rights and limitations under the License.
 
 The Original Code is install.pl.
 
-The Initial Developer of the Original Code is Research Foundation of State University of New York, on behalf of University at Buffalo.
+The Initial Developer of the Original Code is Research Foundation of State
+University of New York, on behalf of University at Buffalo.
 
-Portions created by the Initial Developer are Copyright (C) 2007 Research Foundation of State University of New York, on behalf of University at Buffalo. All Rights Reserved.
+Portions created by the Initial Developer are Copyright (C) 2007 Research
+Foundation of State University of New York, on behalf of University at Buffalo.
+All Rights Reserved.
 
-Contributor(s): Chris Forstall
+Contributor(s): Chris Forstall <cforstall@gmail.com>
 
-Alternatively, the contents of this file may be used under the terms of either the GNU General Public License Version 2 (the "GPL"), or the GNU Lesser General Public License Version 2.1 (the "LGPL"), in which case the provisions of the GPL or the LGPL are applicable instead of those above. If you wish to allow use of your version of this file only under the terms of either the GPL or the LGPL, and not to allow others to use your version of this file under the terms of the UBPL, indicate your decision by deleting the provisions above and replace them with the notice and other provisions required by the GPL or the LGPL. If you do not delete the provisions above, a recipient may use your version of this file under the terms of any one of the UBPL, the GPL or the LGPL.
+Alternatively, the contents of this file may be used under the terms of either
+the GNU General Public License Version 2 (the "GPL"), or the GNU Lesser General
+Public License Version 2.1 (the "LGPL"), in which case the provisions of the
+GPL or the LGPL are applicable instead of those above. If you wish to allow use
+of your version of this file only under the terms of either the GPL or the
+LGPL, and not to allow others to use your version of this file under the terms
+of the UBPL, indicate your decision by deleting the provisions above and
+replace them with the notice and other provisions required by the GPL or the
+LGPL. If you do not delete the provisions above, a recipient may use your
+version of this file under the terms of any one of the UBPL, the GPL or the
+LGPL.
 
 =cut
 
@@ -194,7 +210,7 @@ create_php_defs(catfile($fs{html}, 'defs.php'));
 # install documentation
 #
 
-my $file_script = catfile($fs{script}, 'doc_gen.pl');
+$file_script = catfile($fs{script}, 'doc_gen.pl');
 $file_script = Tesserae::escape_path($file_script);
 `$Tesserae::perl_path $file_script`;
 
