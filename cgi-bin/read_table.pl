b7b8a455af5ba807bbb289a899b061ed244783a0
Gawley <jamesgaw@cast-clgenusem.caset.buffalo.edu>
2017-09-05 15:44:25 -0400

    Bug fix in CGI read-table export variable

--- cgi-bin/read_table.pl
+++ cgi-bin/read_table.pl
@@ -429,7 +429,8 @@ else {
 	$multi_cutoff    = $query->param('mcutoff')      || $multi_cutoff;
 	@include         = $query->param('include');
 	$recall_cache    = $query->param('recall_cache') || $recall_cache;
-	
+	$export    = $query->param('export') || $export;
+		
 	unless (defined $source) {
 	
 		die "read_table.pl called from web interface with no source";
